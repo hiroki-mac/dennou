e6049ecfb81e1b50a87ef46e94ca796b05e2a0af
Hiroki Kashimura <hiroki@gfd-dennou.org>
2018-05-07 16:46:45 +0900

    try 7.2.3

--- dclc.rb
+++ dclc.rb
@@ -1,14 +1,14 @@
 class Dclc < Formula
   desc "DCLC is a C version of DCL, a drawing library useful for planetary and Earth sciences. This library includes graphical, character processing, and mathematical libraries."
   homepage "http://www.gfd-dennou.org/library/dcl/"
-  url "http://www.gfd-dennou.org/library/dcl/dcl-7.3.1-C.tar.gz"
-  sha256 "3a1fdadf40878d52580647e80a769af927318bcb58943284bed432058b2c702c"
+  url "http://www.gfd-dennou.org/library/dcl/dcl-7.2.3-C.tar.gz"
+  sha256 "cdabede9ed4ff8ded349c8f6d35c036191935d004e7c1fee4b6e5cc842eaba25"
   depends_on "gtk+"
   depends_on "pkg-config"
 
   def install
     ENV.deparallelize
-    system "./configure", "--prefix=#{prefix}", "--with-gtk2"
+    system "./configure", "--prefix=#{prefix}"
     system "make"
     system "make install"
   end
