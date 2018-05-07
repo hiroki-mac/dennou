961650fb8bdd10e04275b1da376c86ed1b475a2d
Hiroki Kashimura <hiroki@gfd-dennou.org>
2018-05-07 16:39:47 +0900

    changed to --with-gtk2

--- dclc.rb
+++ dclc.rb
@@ -3,12 +3,12 @@ class Dclc < Formula
   homepage "http://www.gfd-dennou.org/library/dcl/"
   url "http://www.gfd-dennou.org/library/dcl/dcl-7.3.1-C.tar.gz"
   sha256 "3a1fdadf40878d52580647e80a769af927318bcb58943284bed432058b2c702c"
-  depends_on "gtk+3"
+  depends_on "gtk+"
   depends_on "pkg-config"
 
   def install
     ENV.deparallelize
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}", "--with-gtk2"
     system "make"
     system "make install"
   end
